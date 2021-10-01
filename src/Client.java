6c21962fe08079442faac73498b0041e13da908e
Chong Chen <chong.chen@seamoney.com>
2021-10-01 23:21:50 +0800

    Implement arguments check

--- src/Client.java
+++ src/Client.java
@@ -1,2 +1,56 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Client {
+
+    /** IP Address and port of the Server */
+    private final String address;
+    private final int port;
+
+    /** Initialize socket and output streams */
+    private Socket socket = null;
+    private PrintWriter out = null;
+
+
+    Client(String address, int port, int time) {
+        this.address = address;
+        this.port = port;
+    }
+
+    /**
+     * Start the Client
+     */
+    void start() {
+        try {
+            // Establish a connection
+            socket = new Socket(address, port);
+            System.out.println("Connected to the Server");
+
+            // Create a writer object
+            out = new PrintWriter(socket.getOutputStream(), true);
+
+            // Take the message from the user
+            System.out.println("Type your message below ..");
+            Scanner scr = new Scanner(System.in);
+            String line = scr.nextLine();
+
+            // Send the message
+            out.println(line);
+            System.out.println("Message sent");
+
+            // Close the connection
+            System.out.println("Closing connection");
+            out.close();
+            socket.close();
+        } catch(IOException u) {
+            System.out.println(u);
+            System.exit(1);
+        }
+    }
+
+    private void printSummary() {
+
+    }
 }
--- src/Iperfer.java
+++ src/Iperfer.java
@@ -1,5 +1,97 @@
-public class Iperfer {
-    public static void main(String[] args) {
+import java.io.IOException;
 
+public class Iperfer {
+    private static final String USAGE_INFO = "Error: invalid arguments\nClient Mode: java Iperfer -c -h <server hostname> -p <server port> -t <time>\nSerevr Mode: java Iperfer -s -p <listen port>\n";
+    private static final int PORT_MIN = 1024;
+    private static final int PORT_MAX = 65535;
+    private static final int DUMMY_INT = -1;
+
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            printUsageAndExit();
+        }
+
+        String runMode = args[0];
+
+        if (runMode == "-c") {
+            if (args.length != 7) {
+                printUsageAndExit();
+            }
+            if (args[1] == "-h" && args[3] == "-p" && args[5] =="-t") {
+                String serverHostname = args[2];
+                int serverPort = parsePort(args[4]);
+                int time = parseTime(args[5]);
+                if (serverPort == DUMMY_INT || time == DUMMY_INT) {
+                    System.exit(-1);
+                }
+                clientMode(serverHostname, serverPort, time);
+            } else {
+                printUsageAndExit();
+            }
+        }
+
+        else if (runMode == "-s") {
+            if (args.length != 3) {
+                printUsageAndExit();
+            }
+            if (args[1] == "-p") {
+                int listenPort = parsePort(args[2]);
+                if (listenPort == DUMMY_INT) {
+                    System.exit(-1);
+                }
+                serverMode(listenPort);
+            } else {
+                printUsageAndExit();
+            }
+        }
+
+        else {
+            printUsageAndExit();
+        }
+    }
+
+    private static void clientMode(String serverHostname, int serverPort, int time) {
+        Client client = new Client(serverHostname, serverPort, time);
+        client.start();
+    }
+
+    private static void serverMode(int listenPort) {
+        Server server = new Server(listenPort);
+        server.start();
+    }
+
+    private static void printUsageAndExit() {
+        System.out.print(USAGE_INFO);
+        System.exit(-1);
+    }
+
+    private static int parsePort(String portStr) {
+        try {
+            int port = Integer.parseInt(portStr);
+            if (port >= PORT_MIN && port <= PORT_MAX) {
+                return port;
+            } else {
+                System.out.printf("Error: port number must be in the range %d to %d\n", PORT_MIN, PORT_MAX);
+                return DUMMY_INT;
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Error: port should be an int");
+            return DUMMY_INT;
+        }
+    }
+
+    private static int parseTime(String timeStr) {
+        try {
+            int port = Integer.parseInt(timeStr);
+            if (port > 0) {
+                return port;
+            } else {
+                System.out.println("Error: time should be positive int");
+                return DUMMY_INT;
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Error: time should be an int");
+            return DUMMY_INT;
+        }
     }
 }
